5e7801a555b34c91a2805fe4db512006c4d190a2
Justin Signo <justinsigno@yahoo.com>
2024-08-18 15:29:07 -0400

    locations replaced about

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,7 +1,7 @@
 export const navItems = [
   { label: "Home", href: "#" },
   { label: "Services", href: "#" },
-  { label: "About", href: "#" },
+  { label: "Locations", href: "#" },
   { label: "Contact", href: "#" },
 ];
 
