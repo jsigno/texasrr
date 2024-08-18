8921b44b7dfccad01e9f4e9a9d7516d1236da3d6
Justin Signo <justinsigno@yahoo.com>
2024-08-18 16:23:36 -0400

    footer testing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,17 @@ import logo from "../assets/logo.png";
 const Footer = () => {
   return (
     <footer className="mt-20 border-t py-10 border-neutral-700">
-      <div className="px-4 grid grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="px-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
       {/* <img className="h-48 mr-2" src={logo} alt="Logo" /> */}
         <div>
           <h3 className="text-md font-semibold mb-4">Socials</h3>
           <ul className="space-y-2">
-            {socialLinks.map((social, index) => (
+            {contactLinks.map((contact, index) => (
               <li key={index}>
                 <a
-                  href={social.href}
+                  href={contact.href}
                 >
-                  {social.text}
+                  {contact.text}
                 </a>
               </li>
             ))}
@@ -23,12 +23,12 @@ const Footer = () => {
         <div>
           <h3 className="text-md font-semibold mb-4">Contact Us</h3>
           <ul className="space-y-2 px">
-            {contactLinks.map((contact, index) => (
+            {socialLinks.map((links, index) => (
               <li key={index}>
                 <a
-                  href={contact.href}
+                  href={links.href}
                 >
-                  {contact.text}
+                  {links.text}
                 </a>
               </li>
             ))}
